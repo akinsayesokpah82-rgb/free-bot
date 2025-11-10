939baf26319465a875e1a856103e9edca63df36f
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-10 21:08:22 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Static frontend
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -21,8 +19,8 @@ app.get("/api", (req, res) => {
   res.json({ message: "Backend is running successfully ✅" });
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+);
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
