3cf03d1db58803c951f16ec6aa3cfe40d2e2e023
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-10 18:31:28 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   build: {
-    outDir: "dist",
+    outDir: path.resolve(__dirname, '../backend/frontend/dist'),
+  },
+  server: {
+    port: 5173,
   },
 });
